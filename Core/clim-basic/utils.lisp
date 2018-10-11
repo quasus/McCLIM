3711f76b9938b835305adb25cd5f4d1f6aa7f43c
Daniel Kochmanski <daniel@turtleware.eu>
2018-10-11 11:19:47 +0200

    fonts: numerous improvements
    - climb:font-text-extents conforms to the new api
    - clx's and renderers text-size and text-bounding-box use the new
      font-text-extents
    - glyph l0 cache has been removed (our codes are made of two characters now, so
      cache of size 512 is useless, virtually any code except singleton letters
      falls outside it)

--- Core/clim-basic/utils.lisp
+++ Core/clim-basic/utils.lisp
@@ -463,7 +463,7 @@ STREAM in the direction DIRECTION."
   (etypecase specification
     (integer specification)
     ((or string character) (multiple-value-bind (width height)
-                               (text-size stream specification)
+                               (text-size stream (string specification))
                              (ecase direction
                                (:horizontal width)
                                (:vertical height))))
